c4210828a50d5abb3aadf9562c4f468ac497eefe
Anthony Veaudry <anthony@veaudry.pro>
2019-01-20 22:32:42 +0200

    initial commit
    Signed-off-by: Anthony Veaudry <anthony@veaudry.pro>

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import axios from "axios";
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    // this.toggle = this.toggle.bind(this);
+    this.state = {
+      results: []
+    };
+  }
+
+
+opap = () => {
+  axios({
+    method:'get',
+    url: 'https://api.opap.gr/draws/v3.0/1100/last-result-and-active?includeAnimation=true&status=results&status=pendingResults'
+  })
+  .then( (response) => {
+    this.setState({
+      results: response.data.last.winningNumbers.list.sort((a, b) => a - b)
+    });
+    console.log(response.data.last.winningNumbers.list)
+  });
+}
+
+componentDidMount() {
+  this.opap()
+  setInterval( () => {
+    this.opap();
+  }, 500);
+}
+
+
+  render() {
+    const { results } = this.state;
+    return (
+      <div className="App">
+        <ul>
+         {
+           results.map((item, i) => <li key={i}> {item} </li>)
+         }
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default App;
