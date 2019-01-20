85b09b86404a39ca411a6ed608eda385888e3858
Anthony Veaudry <anthony@veaudry.pro>
2019-01-20 22:25:26 +0200

    Updates

---
+++ precache-manifest.a568d7a19740b51401f30f9b7c590ef1.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "be52ba01a4b282d149a4",
+    "url": "/ranomapi/static/js/main.be52ba01.chunk.js"
+  },
+  {
+    "revision": "0f65daa1205e4afb7f71",
+    "url": "/ranomapi/static/js/1.0f65daa1.chunk.js"
+  },
+  {
+    "revision": "bde71e2ba5483196ffdc",
+    "url": "/ranomapi/static/js/runtime~main.bde71e2b.js"
+  },
+  {
+    "revision": "d00f37cd18ac3c3df9e132d4474d3ab7",
+    "url": "/ranomapi/index.html"
+  }
+];
